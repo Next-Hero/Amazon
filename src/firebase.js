fa764a2331104d28e31614f2ce8f70f8f1170e29
nobel7761 <habiburnobel@gmail.com>
2022-05-01 02:57:40 +0600

    project folder structured

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAb-d434OPR5cb1cY9pweZUw8tBeZvkCNQ",
+    authDomain: "clone-53e57.firebaseapp.com",
+    projectId: "clone-53e57",
+    storageBucket: "clone-53e57.appspot.com",
+    messagingSenderId: "629946518210",
+    appId: "1:629946518210:web:71b9d452c0911821d0095b",
+    measurementId: "G-YHNNCS0HWB"
+};
